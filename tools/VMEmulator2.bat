0a7b47d1a939589d7ca601007bf615fbdd294b7d
victorxheng <victorchengthesecond@gmail.com>
2022-09-12 21:05:40 -0700

    load in files

---
+++ tools/VMEmulator2.bat
@@ -0,0 +1,1 @@
+"%JAVA_HOME%/bin/javaw.exe" -classpath "%CLASSPATH%;.;bin/classes;bin/lib/Hack.jar;bin/lib/HackGUI.jar;bin/lib/Simulators.jar;bin/lib/SimulatorsGUI.jar;bin/lib/Compilers.jar" VMEmulatorMain
